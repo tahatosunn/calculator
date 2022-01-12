2fd28f94adc1678534df933ece9a6c6a7f4fd75a
Taha Tosun <taha.tosun@kokteyl.com>
2022-01-12 11:36:49 +0300

    initial project

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Taha Tosun on 12.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
